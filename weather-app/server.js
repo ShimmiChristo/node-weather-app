ebdfff7c35ad41167adf803273a8ef49c79daa04
shimmiChristo <shimmin.christopher@gmail.com>
2019-01-07 19:55:26 -0500

    setting up the post route

--- weather-app/server.js
+++ weather-app/server.js
@@ -1,12 +1,18 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
 app.set('view engine', 'ejs')
 
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.render('index')
 })
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
+})
+
+app.post('/', function(req, res) {
+  res.render('index');
 })
